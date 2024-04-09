2ec66a9393a28068300331ee75be9ab7e06782a4
Tan Chun Siong <dreamtcs@hotmail.com>
2024-04-09 14:01:50 +0800

    changed project output directory and added work in progress dockerfile for Windows

--- LocalRecording/MSDK_LocalRecording.cpp
+++ LocalRecording/MSDK_LocalRecording.cpp
@@ -46,7 +46,7 @@ string video_source = "";
 constexpr auto DEFAULT_VIDEO_SOURCE = "Big_Buck_Bunny_1080_10s_1MB.mp4";
 constexpr auto CONFIG_FILE = "config.json";
 
-bool isJWTWebService = false;
+bool isJWTWebService = true;
 
 
 IMeetingRecordingController* m_pRecordController;
